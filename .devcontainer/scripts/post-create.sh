4f5a3208fe90becb4a3e9787a546e64639b0699b
Mihail Fox <foxmihail83@gmail.com>
2025-10-04 20:22:53 +0000

    chore: tune devcontainer setup scripts

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -4,4 +4,5 @@ echo "This script is run after update-content.sh"
 curl -L --fail --show-error --progress-bar -o ~/.local/bin/codex \
   https://github.com/openai/codex/releases/latest/download/codex && \
   chmod +x ~/.local/bin/codex
-  
+
+uv tool install --force specify-cli --from git+https://github.com/github/spec-kit.git
